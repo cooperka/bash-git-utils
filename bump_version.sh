aa3aea96589cd93fd33d4fcbed17969c9be39611
Kevin Cooper <cooperka92@gmail.com>
2016-11-10 12:56:07 -0500

    :package: Minor refactor to make names less confusing

--- bump_version.sh
+++ bump_version.sh
@@ -4,20 +4,20 @@
 #
 # @param $1 New version number to bump to.
 
+# Validate input.
+if [[ ! "$1" =~ ^[0-9\.]+$ ]]; then
+  echo "Invalid params."
+  echo "Usage: $(basename $0) <version>, e.g. $(basename $0) 1.0.2"
+  exit 1
+fi
+
 # Change to the directory where this script is stored.
 cd "$(dirname ${BASH_SOURCE[0]})"
 
 # Load common methods to call.
 source ./git_utils
 
-# Validate input.
-if [[ ! "$1" =~ ^[0-9\.]+$ ]]; then
-  echo "Invalid params."
-  echo "Usage: ./$(basename $0) <version>, e.g. ./$(basename $0) 1.0.2"
-  exit 1
-fi
-
-init "About to bump version to $1 and commit locally."
+init_safe "About to bump version to $1 and commit locally."
 
 echo;
-bump_version "$1" "${PWD}"
+perform_version_bump "$1" "${PWD}"
--- cut_release.sh
+++ cut_release.sh
@@ -6,18 +6,18 @@
 # @param $1 New version number to bump to.
 # @param $2 (optional) Extra string to add as a tag suffix.
 
+# Validate input.
+if [[ "$#" -lt 1 || "$#" -gt 2 ]] || [[ ! "$1" =~ ^[0-9\.]+$ ]]; then
+  echo "Invalid params."
+  echo "Usage: $(basename $0) <version> [<tag-suffix>], e.g. $(basename $0) 1.0.2 rc1"
+  exit 1
+fi
+
 # Change to the directory where this script is stored.
 cd "$(dirname ${BASH_SOURCE[0]})"
 
 # Load common methods to call.
 source ./git_utils
-
-# Validate input.
-if [[ "$#" -lt 1 || "$#" -gt 2 ]] || [[ ! "$1" =~ ^[0-9\.]+$ ]]; then
-  echo "Invalid params."
-  echo "Usage: ./$(basename $0) <version> [<tag-suffix>], e.g. ./$(basename $0) 1.0.2 rc1"
-  exit 1
-fi
 
 # Update and push new version name.
 init_with_warning "About to cut release version $1 from master."
@@ -29,6 +29,7 @@ if [ "$2" ]; then
 else
   tag_string="v$1"
 fi
+
 push_tag "${tag_string}"
 
 echo;
